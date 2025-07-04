674388a05ff198b8427dd7b6eab319aa28e33f06
Nisarg_Fedora <amlaninisarg15@gmail.com>
2025-07-04 22:31:02 +0530

    Failing Test for Task 1

--- __test__/calculator.spec.js
+++ __test__/calculator.spec.js
@@ -1,8 +1,16 @@
-
-
-describe('Basic Test',() => {
-    test('test-1 : return 0 for empty string',() => {
+describe('Basic Test', () => {
+    test('test-1 : return 0 for empty string', () => {
         const calc = new StringCalculator()
         expect(calc.add('')).toBe(0);
+    })
+
+    test('test-2 : return number for single input', () => {
+        const calc = new StringCalculator()
+        expect(calc.add('1')).toBe(1)
+    })
+
+    test('test-3 : return sum of two numbers',() => {
+        const calc = new StringCalculator()
+        expect(calc.add('1,2').toBe(3))
     })
 })
