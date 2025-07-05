5aa74d1aa094837f167bb47270a417a63119f088
Nisarg_Fedora <amlaninisarg15@gmail.com>
2025-07-05 15:30:52 +0530

    Updated Add method for Task 9 :- number >1000 should be ignored

--- src/calculator.js
+++ src/calculator.js
@@ -4,17 +4,24 @@ class StringCalculator {
             return 0;
 
         let delimiter = ',';
+        let negatives = []
+            if (numbers.substring(0, 2) === "//") {
+                delimiter = numbers[2];
+                numbers = numbers.substring(4);
+            }
+            numbers = numbers.replace(/\n/g, delimiter);
+            let nums = numbers.split(delimiter);
 
-        if (numbers.substring(0, 2) === "//") {
-            delimiter = numbers[2];
-            numbers = numbers.substring(4);
-        }
-        numbers = numbers.replace(/\n/g, delimiter);
-        let nums = numbers.split(delimiter);
+            let answer = nums.reduce((sum, num) => {
+                let n = parseInt(num)
+                if(n < 0) negatives.push(n)
+                if(n>1000) return sum
+                return sum + n;
+            }, 0);
 
-        return nums.reduce((sum, num) => {
-            return sum + parseInt(num);
-        }, 0);
+            if(negatives.length >= 1) throw new Error(`negatives not allowed : ${negatives.join(',')}`)
+            return answer
+
     }
 }
 
